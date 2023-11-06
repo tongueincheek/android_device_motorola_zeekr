c2a55300230aa545e5e8c5e2253bff29a6fa7d3e
lexingtontezz <tezz.dixon@outlook.com>
2023-11-06 10:27:28 -0500

    tree: various changes, nothing too important

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,13 @@
 #
 
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_zeekr.mk \
     $(LOCAL_DIR)/twrp_zeekr.mk
 
 COMMON_LUNCH_CHOICES := \
+    lineage_zeekr-user \
+    lineage_zeekr-userdebug \
+    lineage_zeekr-eng \
     twrp_zeekr-user \
     twrp_zeekr-userdebug \
     twrp_zeekr-eng
